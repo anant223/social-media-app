1bddc4f7184e2beadc36afbcbd7f3aa2efedb65f
Anant <p.anant223@gmail.com>
2023-01-05 01:29:18 +0530

    creating posts and styling them

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,19 @@
+const Post = require("../models/post")
+
 module.exports.home = function(req, res){
-    console.log(req.cookies)
-    res.cookie('user_id', 25)
-    return res.render('home', {
-        title: "Codeial"
+    // console.log(req.cookies)
+    // res.cookie('user_id', 25)
+    // Post.find({}, function(err, posts){ })
+    
+    // populate user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render("home", {
+          title: "Codeial",
+          posts: posts,
+        });
     })
+   
 }
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require("../controllers/home_controller")
 
 router.get("/", homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 //for any further routes, access from here
 // router.use('/routerName', require('./routerfile'))
